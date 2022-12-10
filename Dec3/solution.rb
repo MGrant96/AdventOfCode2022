36068ccbb567d8b269eae664e7042f515002674a
Michael <michaelgrant13@hotmail.co.uk>
2022-12-10 01:43:17 +0000

    Day 3 task 1 done

--- Dec3/solution.rb
+++ Dec3/solution.rb
@@ -1,2 +1,25 @@
 #!/usr/bin/env ruby
 
+overall_sum = 0
+value = 0
+
+File.readlines('dec3input.txt').each do |line|
+  half_way = (line.length / 2) - 1
+
+  one = line[0..half_way]
+  two = line[half_way + 1..line.length]
+    one.split('').each do |l| # can also use this:next unless two.include?(l)
+        if two.include?(l)
+            if l == l.downcase
+                value = l.ord - 96
+            else
+                value = l.ord - 38
+            end
+          overall_sum += value
+    
+          break
+        end
+    end
+end
+
+puts overall_sum
